87e5e51b0c459ec2a1811aa4704628268929dbda
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-10 14:44:38 -0800

    Add fibonacci checker function
    The function will take an argument and return the value of the number
    at that location in the fibonacci sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS"
+    };
+    number = Number(number);
+    let sum = [1, 1];
+    for (let i = 1; i <= number; i++) {
+        sum.push(sum[i] + sum[i-1]);
+    }
+
+    return sum[number-1];
 
 };
 
