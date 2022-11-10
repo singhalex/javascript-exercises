1fc408bb5e5cb45a1a14b3665e9b5c0534df078c
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-10 09:31:46 -0800

    Add function to repeat starting
    The function repeats the string by a user defined amount.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        let repeatedString = "";
+        for (i = 0; i < num; i++) {
+            repeatedString += string;
+        }
+        return repeatedString;
+    }
 
 };
 
