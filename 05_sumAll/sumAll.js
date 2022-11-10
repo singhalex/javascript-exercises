999f3a4d09ef4e7d05180314edbd4442c8327dd7
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-10 12:59:29 -0800

    Add function to sum numbers
    Function will sum two numbers passed by the user and all numbers in
    between.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    let start = 0;
+    let end = 0;
 
+    if (firstNumber < 0 || secondNumber < 0 || typeof firstNumber !== "number" || typeof secondNumber !== "number") {
+        return "ERROR"
+    }
+
+    if (firstNumber > secondNumber) {
+        start = secondNumber;
+        end = firstNumber;
+    } else {
+        start = firstNumber;
+        end = secondNumber;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
