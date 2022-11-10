9fd96a8c4fb33f0d4992832e092833c926aa5256
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-10 14:21:02 -0800

    Add function to check palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[^\w\']|_/g, "")
+    if (word === word.split("").reverse().join("")) {
+        return true
+    } else {
+        return false
+    }
 
 };
 
