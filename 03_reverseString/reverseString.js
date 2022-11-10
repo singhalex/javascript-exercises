ce608aa6aa7131974aede8034fb957ec78e196a9
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-10 10:04:33 -0800

    Add function to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let backwardsString = "";
+    let letters = string.split("");
+    
+    for (i = 0; i < string.length; i++) {
+        backwardsString += letters.pop();
+    }
 
+    return backwardsString
 };
 
 // Do not edit below this line
