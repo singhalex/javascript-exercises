c2c1822ccbaee7ed80143de70bbcb28a2b4756af
Alex Singh <115970252+singhalex@users.noreply.github.com>
2022-11-11 15:14:22 -0800

    Add function to determine older person in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(birthRecords) {
+    let oldestPerson = "";
+    let oldestAge = 0;
+    let currentYear = new Date().getFullYear();
 
+    for (person of birthRecords) {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = currentYear;
+        }
+        if (person.yearOfDeath - person.yearOfBirth > oldestAge) {
+            oldestAge = person.yearOfDeath - person.yearOfBirth;
+            oldestPerson = person;
+        }
+    };
+
+    return oldestPerson     
 };
 
 // Do not edit below this line
